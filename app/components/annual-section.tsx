8e194f9fa51b29f5359f374b6629ddb887b133de
Mihajatiana <miandriakotonerason@gmail.com>
2025-11-18 14:06:32 +0300

    add: image section to annual

--- app/components/annual-section.tsx
+++ app/components/annual-section.tsx
@@ -1,12 +1,26 @@
+import Image from "next/image";
+
 export default function AnnualSection() {
   return (
-    <div className="flex flex-col gap-10 items-center px-10 pt-11 pb-32">
-      <p>33RD ANNUAL LANTERN FESTIVAL</p>
-      <h3 className="text-5xl text-center font-semibold">
-        We invite you to our enchanting festival, where light, creativity, and
-        tradition come together to illuminate the night sky in a breathtaking
-        celebration of culture and artistry.
-      </h3>
+    <div>
+      <div className="flex flex-col gap-10 items-center px-10 pt-11 pb-32">
+        <p>33RD ANNUAL LANTERN FESTIVAL</p>
+        <h3 className="text-5xl text-center font-semibold">
+          We invite you to our enchanting festival, where light, creativity, and
+          tradition come together to illuminate the night sky in a breathtaking
+          celebration of culture and artistry.
+        </h3>
+      </div>
+
+      <div className="flex justify-center px-10 pb-36">
+        <Image
+          src={"/images/annual.png"}
+          alt="Annual image"
+          width={1900}
+          height={1080}
+          className="w-full h-auto"
+        />
+      </div>
     </div>
   );
 }
