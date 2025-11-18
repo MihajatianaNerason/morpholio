a3d698bf718c11958bd39c44090e3644ba3578b3
Mihajatiana <miandriakotonerason@gmail.com>
2025-11-18 13:55:40 +0300

    add: annual section

---
+++ app/components/annual-section.tsx
@@ -0,0 +1,12 @@
+export default function AnnualSection() {
+  return (
+    <div className="flex flex-col gap-10 items-center px-10 pt-11 pb-32">
+      <p>33RD ANNUAL LANTERN FESTIVAL</p>
+      <h3 className="text-5xl text-center font-semibold">
+        We invite you to our enchanting festival, where light, creativity, and
+        tradition come together to illuminate the night sky in a breathtaking
+        celebration of culture and artistry.
+      </h3>
+    </div>
+  );
+}
