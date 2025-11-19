44ff1a591e2aa19883ea2736a6f501bc99753c0f
Mihajatiana <miandriakotonerason@gmail.com>
2025-11-19 14:20:55 +0300

    add: footer section

---
+++ app/components/footer.tsx
@@ -0,0 +1,29 @@
+import { Facebook, Instagram } from "lucide-react";
+
+export default function Footer() {
+  return (
+    <div className="bg-[#7AA4AD] p-10 flex flex-col items-center gap-44">
+      <div className="w-full md:px-2.5 flex flex-col gap-20 md:flex-row md:justify-between">
+        <div className="font-bold">
+          <p>Westwood Crest Middle School</p>
+          <p>2415 Candyland Lane</p>
+          <p>Los Angeles, CA 90025</p>
+        </div>
+        <div className="font-bold">
+          <p>PAPER LANTERNS &copy; 2025</p>
+          <p>HELLO@MOOD.COM</p>
+          <p>646.555.1234</p>
+        </div>
+        <div className="flex gap-3">
+          <Facebook fill="black" />
+          <Instagram />
+        </div>
+      </div>
+      <div>
+        <h2 className="text-[#FBEAD6] text-7xl md:text-9xl font-extrabold text-center">
+          PAPER LANTERNS
+        </h2>
+      </div>
+    </div>
+  );
+}
