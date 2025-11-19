b6f7cfce862e6c056fbd9d864a7ded652d613c47
Mihajatiana <141212483+MihajatianaNerason@users.noreply.github.com>
2025-11-19 14:35:41 +0300

    add: footer section (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import AnnualSection from "./components/annual-section";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <div className="">
       <AnnualSection />
+
+      <Footer />
     </div>
   );
 }
